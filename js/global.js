400704a08eb773d99feda2b64056c865155362cb
Tom Rankin <tom@tomrankin.net>
2016-04-05 13:57:19 -0500

    git graph starting

--- js/global.js
+++ js/global.js
@@ -1,3 +1,11 @@
+var feature1Col = 0;
+var feature2Col = 1;
+var developCol = 2;
+var releaseCol = 3;
+var masterCol = 4;
+var hotfixCol = 5;
+
+
 var j = 0;
 window.addEventListener("click", function(){
 
